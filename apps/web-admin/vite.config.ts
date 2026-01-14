fcebe010bf94d30c74ce4814fdb953fc673d3cec
小可爱天下无敌 <3292339490@qq.com>
2026-01-14 18:01:44 +0800

    feat(web-admin): 集成 React 编译器并优化构建配置
    - 添加 babel-plugin-react-compiler 依赖以启用 React 编译器功能
    - 修改 Vite 配置，在 React 插件中集成 Babel 插件
    - 将 path 模块导入从 path 改为 resolve 方法以优化路径处理
    - 配置 Rollup 构建选项，实现静态资源分类打包
    - 设置 chunk、entry 和 asset 文件命名规则，优化资源加载性能

--- apps/web-admin/vite.config.ts
+++ apps/web-admin/vite.config.ts
@@ -1,13 +1,33 @@
 import { defineConfig } from 'vite'
-import path from "path";
+import { resolve } from "path";
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    babel: {
+      plugins: [
+        "babel-plugin-react-compiler"
+      ]
+    }
+  })],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, '', 'index.html')
+      },
+      // 静态资源分类打包
+      output: {
+        chunkFileNames: "static/js/[name]-[hash].js",
+        entryFileNames: "static/js/[name]-[hash].js",
+        assetFileNames: "static/[ext]/[name]-[hash].[ext]",
+        
+      },
     },
   },
 })
